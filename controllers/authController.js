9734c4a62db7155c3d564b96f28e60f5ef59f340
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2023-11-15 01:27:15 +0530

    mvc-structure

---
+++ controllers/authController.js
@@ -0,0 +1,17 @@
+import userModel from "../models/userModel";
+export const registerController = (req, res) => {
+  try {
+    const { name, email, password, phone, address } = req.body;
+    //validations
+    if (!name) {
+      return res.send({ error: "Name is required" });
+    }
+  } catch (error) {
+    console.log(error);
+    res.status(500).send({
+      success: false,
+      message: "Error in registeration",
+      error,
+    });
+  }
+};
---
+++ server.js
@@ -0,0 +1,28 @@
+import express from "express";
+import dotenv from "dotenv";
+import morgan from "morgan";
+import connectDB from "./config/db.js";
+import authRoutes from "./routes/authRoute.js";
+//configure env
+dotenv.config();
+const app = express();
+
+//database config
+connectDB();
+
+//middlewares
+app.use(express.json());
+app.use(morgan("dev"));
+
+//routes
+app.use("/api/v1/auth", authRoutes);
+
+app.get("/", (req, res) => {
+  res.send("<h1>Welcome to ecommerce app</h1>");
+});
+const PORT = process.env.PORT || 8080;
+app.listen(PORT, () => {
+  console.log(
+    `Server is running on ${process.env.DEV_MODE} mode on port ${PORT}`
+  );
+});
