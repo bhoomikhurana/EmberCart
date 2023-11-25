00701b4c4785759a59830b06e52e06e061a14d88
bhoomikhurana <96761198+bhoomikhurana@users.noreply.github.com>
2023-11-26 04:20:03 +0530

    admin and user dashboard

---
+++ client/src/pages/Admin/AdminDashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Layout from "../../components/Layout/Layout";
+
+const AdminDashboard = () => {
+  return (
+    <Layout title={"Admin Dahboard EmberCart"}>
+      <h1>Admin dashboard</h1>
+    </Layout>
+  );
+};
+
+export default AdminDashboard;
--- routes/authRoute.js
+++ routes/authRoute.js
@@ -13,7 +13,12 @@ router.post("/login", loginController);
 
 //test route
 router.get("/test", requireSignIn, isAdmin, testController);
+//protected user route auth
 router.get("/user-auth", requireSignIn, (req, res) => {
+  res.status(200).send({ ok: true });
+});
+//protected admin route auth
+router.get("/admin-auth", requireSignIn, isAdmin, (req, res) => {
   res.status(200).send({ ok: true });
 });
 router.post("/forgot-password", forgotPasswordController);
